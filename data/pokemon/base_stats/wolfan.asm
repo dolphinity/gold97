cc3f07236755f8af6eaf089d0709184f6dfbaacb
dolphinity <120517702+dolphinity@users.noreply.github.com>
2023-01-25 16:43:23 -0700

    Update wolfan.asm

--- data/pokemon/base_stats/wolfan.asm
+++ data/pokemon/base_stats/wolfan.asm
@@ -3,17 +3,17 @@
 	db 70,  60,  50,  55,  70,  60
 	;   hp  atk  def  spd  sat  sdf
 
-	db ICE, ICE ; type
-	db 170 ; catch rate
-	db 75 ; base exp
+	db ICE, DARK ; type
+	db 65 ; catch rate
+	db 67 ; base exp
 	db ICE_BERRY, NEVERMELTICE ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 40 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/wolfan/front.dimensions"
 	db 0, 0, 0, 0 ; padding
-	db GROWTH_MEDIUM_FAST ; growth rate
+	db GROWTH_SLOW ; growth rate
 	dn EGG_GROUND, EGG_MONSTER ; egg groups
 
 	; tm/hm learnset
